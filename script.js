e2798ae7cb9efe98fe56c00450bde3f508725d62
Sarah Venske Costa <sarahvenske@gmail.com>
2022-04-18 16:36:49 -0300

    correções feitas

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 //Entrega S03-10 - Funções e Arrays
 
-//Estratégia:
-//Rafael, Manoel e Daniel = Lista de competidores
-//Daniel tem vantagem (1 lugar)
-//Desenvolver a função para criar o podium
+// Estratégia:
+// Rafael, Manoel e Daniel = Lista de competidores
+// Daniel tem vantagem (1 lugar)
+// Desenvolver a função para criar o podium
 
 
-//Se firstPlace = Daniel - Nada acontece;
-//Se secondPlace = Daniel - Inverte com o firstPlace
-//Se lastPlace = Daniel - Inverte com o secondPlace, mantém o firstPlace
+// Se firstPlace = Daniel - Nada acontece;
+// Se secondPlace = Daniel - Inverte com o firstPlace
+// Se lastPlace = Daniel - Inverte com o secondPlace, mantém o firstPlace
 
 
 function positions(firstPlace, secondPlace, lastPlace){
     let positionsArray = [firstPlace, secondPlace, lastPlace]
-
-    if (firstPlace === "Daniel"){
-        podium = firstPlace
+    
+    if(positionsArray[1] == "Daniel"){
+        positionsArray = [secondPlace, firstPlace, lastPlace]
     }
-    else if(secondPlace === "Daniel"){
-        podium = secondPlace
-    }
-    else if(lastPlace === "Daniel"){
-        podium = firstPlace
+    else if(positionsArray[2] == "Daniel"){
+        positionsArray = [firstPlace, lastPlace, secondPlace]
     }   
+console.log('1º colocado: ' + positionsArray[0] + ' 2º colocado: ' + positionsArray[1] + ' 3º colocado: ' + positionsArray[2])
 
-console.log('1o colocado:' + podium)
 }
 
 //Possibilidades de PODIUM:
@@ -34,18 +31,4 @@ positions("Rafael", "Manoel", "Daniel")
 positions("Rafael", "Daniel", "Manoel")
 positions("Manoel", "Daniel", "Rafael")
 positions("Manoel", "Rafael", "Daniel")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
